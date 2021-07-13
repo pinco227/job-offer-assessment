ab068b499d59acc8872315475ae036aab6b9b555
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-13 17:00:22 +0100

    Create model, serializer, views and urls, and test api

--- jobshop/jobs/models.py
+++ jobshop/jobs/models.py
@@ -9,7 +9,7 @@ class JobOffer(models.Model):
     company_email = models.CharField(max_length=100)
     job_title = models.CharField(max_length=120)
     job_description = models.TextField()
-    salary = models.DecimalField(max_digits=6, decimal_places=2)
+    salary = models.DecimalField(max_digits=8, decimal_places=2)
     city = models.CharField(max_length=60)
     state = models.CharField(max_length=60)
     created_at = models.DateTimeField(auto_now_add=True)
